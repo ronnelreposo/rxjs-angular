a7d5312878f5686cc80b0ba4c665e800fadb1ca6
Ronnel Reposo <ronnel.reposo@gmail.com>
2020-01-06 11:30:18 +0800

    Using map operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+/**
+ * Represents a Person model.
+ * */
+type Person = {
+  firstname: string
+  lastname: string
+  age: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    
   title = 'rxjs-angular';
+
+  people$: Observable<Person[]>
+
+  legalAgePeople$: Observable<Person[]>
+
+  ngOnInit(): void {
+
+    const people: Person[] = [
+      { firstname: 'jon', lastname: 'snow', age: 21 },
+      { firstname: 'arya', lastname: 'stark', age: 14 },
+      { firstname: 'tyrion', lastname: 'lannister', age: 32 }
+    ];
+
+
+    // from source array.
+    const people$ = of(people);
+
+
+    // single subscription.
+    people$.subscribe(person => {
+
+      console.log(person);
+    });
+
+    // map - transforming values inside observable.
+    this.people$ = people$.pipe(
+      map(people => {
+        return people.map(person => {
+
+          const personPrime: Person = {
+            firstname: person.firstname.toUpperCase(),
+            lastname: person.lastname.toUpperCase(),
+            age: person.age
+          };
+
+          return personPrime;
+
+        })
+      })
+    );
+
+    this.legalAgePeople$ = people$.pipe(
+      map(people => people.filter(person => person.age > 18))
+    );
+
+
+  }
+
 }
+
+
